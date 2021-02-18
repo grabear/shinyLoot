838491c4ea1a5d000421a258a1d3bd011b454312
Rob Gilmore <robgilmore127@gmail.com>
2021-02-18 11:00:55 -0600

    Added files

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,18 @@
 # This function was inspired from:
 # https://github.com/jennybc/googlesheets/blob/master/R/zzz.R
-.onLoad <- function(libname, pkgname) {
-
-  op <- options()
-  op.shinyLoot <- list(
-    shinyLoot.httr_oauth_cache = TRUE,
-    shinyLoot.webapp.client_id = "498958135112-73skuk874afbk9sdc63tosrppi5lio4n.apps.googleusercontent.com",
-    shinyLoot.webapp.client_secret = "cSX61CJ_PrebbAMX06KNK6mU",
-    shinyLoot.webapp.redirect_uri = "http://127.0.0.1:4642",
-    shiny.port = 4642
-  )
-  toset <- !(names(op.shinyLoot) %in% names(op))
-  if(any(toset)) options(op.shinyLoot[toset])
-
-  invisible()
-
-}
+# .onLoad <- function(libname, pkgname) {
+#
+#   op <- options()
+#   op.shinyLoot <- list(
+#     shinyLoot.httr_oauth_cache = TRUE,
+#     shinyLoot.webapp.client_id = "498958135112-73skuk874afbk9sdc63tosrppi5lio4n.apps.googleusercontent.com",
+#     shinyLoot.webapp.client_secret = "cSX61CJ_PrebbAMX06KNK6mU",
+#     shinyLoot.webapp.redirect_uri = "http://127.0.0.1:4642",
+#     shiny.port = 4642
+#   )
+#   toset <- !(names(op.shinyLoot) %in% names(op))
+#   if(any(toset)) options(op.shinyLoot[toset])
+#
+#   invisible()
+#
+# }
--- inst/shiny-apps/shinyLoot/server.R
+++ inst/shiny-apps/shinyLoot/server.R
@@ -2,12 +2,12 @@ library(shiny)
 library(shinydashboard)
 library(googlesheets)
 
-options(googlesheets.webapp.client_id =  getOption("shinyLoot.webapp.client_id"))
-options(googlesheets.webapp.client_secret = getOption("shinyLoot.webapp.client_secret"))
-options(googlesheets.httr_oauth_cache = getOption("shinyLoot.httr_oauth_cache"))
-options(googlesheets.webapp.redirect_uri = getOption("shinyLoot.webapp.redirect_uri"))
+#options(googlesheets.webapp.client_id =  getOption("shinyLoot.webapp.client_id"))
+# options(googlesheets.webapp.client_secret = getOption("shinyLoot.webapp.client_secret"))
+# options(googlesheets.httr_oauth_cache = getOption("shinyLoot.httr_oauth_cache"))
+# options(googlesheets.webapp.redirect_uri = getOption("shinyLoot.webapp.redirect_uri"))
 
 server <- function(input, output, session) {
   # Initialize reactive values
-  log_status <- reactiveValues(logged_in = FALSE)
+  log_status <- reactiveValues(logged_in = TRUE)
 }
